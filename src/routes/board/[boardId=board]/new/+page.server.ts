0c2721429fdd5277ca91484340ca5ac2d9756c7d
nemoVim <nemovim@kentech.ac.kr>
2025-11-25 12:57:11 +0900

    fix: pathname error 9

--- src/routes/board/[boardId=board]/new/+page.server.ts
+++ src/routes/board/[boardId=board]/new/+page.server.ts
@@ -2,7 +2,7 @@ import BoardService from '$lib/board/service';
 import { fail, redirect } from '@sveltejs/kit';
 
 // The below line is essential to prevent rendering the page without server request which leads to skipping the server hooks.
-// export const load = async () => {};
+export const load = () => {};
 
 export const actions = {
 	createPost: async ({ request, locals, params }) => {
