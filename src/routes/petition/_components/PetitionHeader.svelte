146ba83f40e0053be1b65cde29546fdaa619556a
miniipad03 <miniipad03@kentech.ac.kr>
2026-01-22 18:05:23 +0900

    청원 헤더 html 수정

--- src/routes/petition/_components/PetitionHeader.svelte
+++ src/routes/petition/_components/PetitionHeader.svelte
@@ -8,9 +8,9 @@
 </script>
 
 <header class="container-col module_head">
-    <div class="container">
-        <h1>청원</h1>
-        
+    <h1>청원</h1>
+	<div class="container">
+		<p>더 나은 학교를 위해 소중한 의견을 제안해 주세요</p>
         {#if pageType === 'list'}
             <LinkButton href="/petition/new">
                 <Pen size="1rem" />
@@ -24,9 +24,6 @@
             </LinkButton>
         {/if}
     </div>
-	<p class="notice">
-        더 나은 학교를 위해 소중한 의견을 제안해 주세요
-    </p>
     <hr />
 </header>
 
