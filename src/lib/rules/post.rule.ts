c27e9f7ef53bc926f3bc664a3b1404f86f14ad7e
miniipad03 <miniipad03@kentech.ac.kr>
2026-06-02 02:35:37 +0900

    refactor: canCreatePost uses BOARD_CONFIG.writeMinRole

--- src/lib/rules/post.rule.ts
+++ src/lib/rules/post.rule.ts
@@ -1,15 +1,12 @@
 import type { User } from '$lib/types/user.type.js';
 import type { Post, PostCreate } from '$lib/types/post.type.js';
-import { BoardId } from '$lib/types/board.type.js';
-import { UserGroup } from '$lib/types/user.type.js';
+import { BOARD_CONFIG } from '$lib/types/board.type.js';
 import { hasMinRole } from '$lib/common/permission.js';
 
 export function canEditOrDeletePost(post: Post, user: User): boolean {
-	return post.userId === user._id || hasMinRole(user, UserGroup.Moderator);
+	return post.userId === user._id || hasMinRole(user, 'moderator');
 }
 
 export function canCreatePost(post: PostCreate, user: User): boolean {
-	if (post.boardId === BoardId.Notice || post.boardId === BoardId.Bylaw)
-		return hasMinRole(user, UserGroup.Moderator);
-	else return true;
+	return hasMinRole(user, BOARD_CONFIG[post.boardId].writeMinRole);
 }
