80458ff930e73f9afaf8d6a66f7f7d9315eacd55
miniipad03 <miniipad03@kentech.ac.kr>
2026-06-02 02:34:44 +0900

    feat: add BOARD_CONFIG to board.type.ts

--- src/lib/types/board.type.ts
+++ src/lib/types/board.type.ts
@@ -1,3 +1,5 @@
+import { UserGroup } from '$lib/types/user.type.js';
+
 export const BoardId = {
 	Notice: 'notice',
 	Free: 'free',
@@ -5,3 +7,35 @@ export const BoardId = {
 } as const;
 
 export type BoardId = (typeof BoardId)[keyof typeof BoardId];
+
+interface BoardConfig {
+	label: string;
+	description: string;
+	allowComments: boolean;
+	allowLikes: boolean;
+	writeMinRole: UserGroup;
+}
+
+export const BOARD_CONFIG: Record<BoardId, BoardConfig> = {
+	[BoardId.Notice]: {
+		label: '공지사항',
+		description: '학생회의 공지사항을 한눈에 확인하세요',
+		allowComments: false,
+		allowLikes: true,
+		writeMinRole: UserGroup.Moderator
+	},
+	[BoardId.Free]: {
+		label: '자유게시판',
+		description: '구성원들과 자유로운 대화를 나눠보세요',
+		allowComments: true,
+		allowLikes: true,
+		writeMinRole: UserGroup.User
+	},
+	[BoardId.Bylaw]: {
+		label: '회칙·세칙',
+		description: '총학생회의 회칙과 세칙을 확인하세요',
+		allowComments: false,
+		allowLikes: true,
+		writeMinRole: UserGroup.Moderator
+	}
+};
