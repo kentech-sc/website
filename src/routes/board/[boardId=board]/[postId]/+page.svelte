e3c08399a00163b689441e042c39503a5c578f0e
miniipad03 <miniipad03@kentech.ac.kr>
2026-06-02 02:39:57 +0900

    feat: hide CommentSection when BOARD_CONFIG.allowComments is false

--- src/routes/board/[boardId=board]/[postId]/+page.svelte
+++ src/routes/board/[boardId=board]/[postId]/+page.svelte
@@ -4,6 +4,8 @@
 	import type { Post } from '$lib/types/post.type.js';
 	import type { Comment } from '$lib/types/comment.type.js';
 	import type { FileMeta } from '$lib/types/file-meta.type.js';
+	import { BOARD_CONFIG } from '$lib/types/board.type.js';
+	import type { BoardId } from '$lib/types/board.type.js';
 
 	import BoardHeader from '../_components/BoardHeader.svelte';
 	import BoardArticle from '../_components/BoardArticle.svelte';
@@ -17,9 +19,14 @@
 	let post = $state<Post>(JSON.parse(data?.post || '{}'));
 	let comments = $derived<Comment[]>(JSON.parse(data?.comments || '[]'));
 	let fileMetas = $derived<Array<FileMeta>>(JSON.parse(data?.files || '[]'));
+
+	const boardId = $derived(page.params.boardId as BoardId);
+	const config = $derived(BOARD_CONFIG[boardId]);
 </script>
 
 <BoardHeader pageType="detail" />
 <BoardArticle bind:post {user} />
 <FileList {fileMetas} isEditing={false} />
-<CommentSection authorId={post.userId} {comments} {user} />
+{#if config.allowComments}
+	<CommentSection authorId={post.userId} {comments} {user} />
+{/if}
