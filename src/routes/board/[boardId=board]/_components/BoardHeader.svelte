283a8092e2598006ce4f513237f2d67101a3a1ef
miniipad03 <miniipad03@kentech.ac.kr>
2026-06-02 02:36:41 +0900

    refactor: BoardHeader uses BOARD_CONFIG for label/description/write permission

--- src/routes/board/[boardId=board]/_components/BoardHeader.svelte
+++ src/routes/board/[boardId=board]/_components/BoardHeader.svelte
@@ -6,31 +6,22 @@
 
 	import LinkButton from '$components/LinkButton.svelte';
 	import { hasMinRole } from '$lib/common/permission';
+	import { BOARD_CONFIG } from '$lib/types/board.type.js';
+	import type { BoardId } from '$lib/types/board.type.js';
 
 	const user = JSON.parse(page.data.user);
 
 	let { pageType } = $props();
-	const boardId = $derived(page.params.boardId);
+	const boardId = $derived(page.params.boardId as BoardId);
+	const config = $derived(BOARD_CONFIG[boardId]);
 </script>
 
 <header class="container-col module_head">
-	{#if boardId === 'free'}
-		<h1>자유게시판</h1>
-	{:else if boardId === 'notice'}
-		<h1>공지사항</h1>
-	{:else if boardId === 'bylaw'}
-		<h1>회칙·세칙</h1>
-	{/if}
+	<h1>{config.label}</h1>
 
 	<div class="container">
-		{#if boardId === 'free'}
-			<p>구성원들과 자유로운 대화를 나눠보세요</p>
-		{:else if boardId === 'notice'}
-			<p>학생회의 공지사항을 한눈에 확인하세요</p>
-		{:else if boardId === 'bylaw'}
-			<p>총학생회의 회칙과 세칙을 확인하세요</p>
-		{/if}
-		{#if pageType === 'list' && (boardId === 'free' || ((boardId === 'notice' || boardId === 'bylaw') && hasMinRole(user, 'moderator')))}
+		<p>{config.description}</p>
+		{#if pageType === 'list' && hasMinRole(user, config.writeMinRole)}
 			<LinkButton href="/board/{boardId}/new">
 				<Pen size="1rem" />
 				<span>글쓰기</span>
