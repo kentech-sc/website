49981a77fdaf6b7941c723c03e96fe7387542668
miniipad03 <miniipad03@kentech.ac.kr>
2026-09-08 03:06:33 +0900

    fix: 배너 목록에서 걸기/삭제가 동작하도록 고친다
    InlineActionForm 의 기본 정책이 inline 이라 액션은 성공해도 목록이 갱신되지
    않아 아무 일도 없는 것처럼 보였다. reload 로 바꾸고, 목록을 업로드 폼 밖으로
    빼서 버튼을 각 배너 항목 안에 둔다. 삭제에는 확인창을 붙였다.

--- src/routes/profile/_components/BannerForm.svelte
+++ src/routes/profile/_components/BannerForm.svelte
@@ -50,6 +50,72 @@
 	}
 </script>
 
+<div class="banner-manager container-col">
+	<h4>
+		<ImageIcon size="0.8rem" />
+		<span>메인 배너</span>
+	</h4>
+
+	<!--
+		각 배너의 버튼은 InlineActionForm(자체 form)이라 아래 업로드 폼 안에 넣을 수 없다.
+		그래서 목록을 업로드 폼 바깥에 두고, 버튼을 해당 항목 안에 붙인다.
+	-->
+	{#if banners.length}
+		<ul class="banner-list">
+			{#each banners as banner (banner.id)}
+				<li class="banner-item" class:active={banner.isActive}>
+					<img src={banner.imagePath} alt={banner.imageAlt} />
+
+					<div class="banner-meta">
+						<span class="ellipsis">
+							{#if banner.isActive}<em>사용 중</em>{/if}
+							{banner.imageAlt}
+						</span>
+						{#if banner.linkUrl}
+							<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -- 관리자가 입력한 외부 주소 -->
+							<a href={banner.linkUrl} target="_blank" rel="noreferrer noopener" class="ellipsis">
+								{banner.linkUrl}
+							</a>
+						{:else}
+							<span class="hint">링크 없음</span>
+						{/if}
+					</div>
+
+					<div class="banner-actions">
+						{#if !banner.isActive}
+							<InlineActionForm
+								actionName="activateBanner"
+								formName="activateBanner"
+								policy="reload"
+								buttonClass="success-btn"
+								hiddenFields={[{ name: 'banner-id', value: banner.id }]}
+							>
+								<Check size="0.8rem" />
+								<span>걸기</span>
+							</InlineActionForm>
+						{/if}
+						<InlineActionForm
+							actionName="removeBanner"
+							formName="removeBanner"
+							policy="reload"
+							buttonClass="error-btn"
+							confirmMessage="이 배너를 삭제할까요? 되돌릴 수 없습니다."
+							hiddenFields={[{ name: 'banner-id', value: banner.id }]}
+						>
+							<Trash2 size="0.8rem" />
+							<span>삭제</span>
+						</InlineActionForm>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		<div class="info">
+			<p>보관함이 비어 있습니다. 이미지를 올리면 바로 메인에 걸립니다.</p>
+		</div>
+	{/if}
+</div>
+
 <CommonForm
 	actionName="addBanner"
 	formName="addBanner"
@@ -57,39 +123,6 @@
 	afterSuccess={handleSuccess}
 >
 	<div class="container-col">
-		<h4>
-			<ImageIcon size="0.8rem" />
-			<span>메인 배너</span>
-		</h4>
-
-		{#if banners.length}
-			<ul class="banner-list">
-				{#each banners as banner (banner.id)}
-					<li class="banner-item" class:active={banner.isActive}>
-						<img src={banner.imagePath} alt={banner.imageAlt} />
-						<div class="banner-meta">
-							<span class="ellipsis">
-								{#if banner.isActive}<em>사용 중</em>{/if}
-								{banner.imageAlt}
-							</span>
-							{#if banner.linkUrl}
-								<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -- 관리자가 입력한 외부 주소 -->
-								<a href={banner.linkUrl} target="_blank" rel="noreferrer noopener" class="ellipsis">
-									{banner.linkUrl}
-								</a>
-							{:else}
-								<span class="hint">링크 없음</span>
-							{/if}
-						</div>
-					</li>
-				{/each}
-			</ul>
-		{:else}
-			<div class="info">
-				<p>보관함이 비어 있습니다. 이미지를 올리면 바로 메인에 걸립니다.</p>
-			</div>
-		{/if}
-
 		<CommonLabel labelFor="banner-image" labelString="배너 이미지">
 			<input
 				type="file"
@@ -126,31 +159,12 @@
 	</div>
 </CommonForm>
 
-{#each banners as banner (banner.id)}
-	<div class="banner-actions">
-		<span class="ellipsis">{banner.imageAlt}</span>
-		{#if !banner.isActive}
-			<InlineActionForm
-				actionName="activateBanner"
-				buttonClass="success-btn"
-				hiddenFields={[{ name: 'banner-id', value: banner.id }]}
-			>
-				<Check size="0.8rem" />
-				<span>이걸로 걸기</span>
-			</InlineActionForm>
-		{/if}
-		<InlineActionForm
-			actionName="removeBanner"
-			buttonClass="error-btn"
-			hiddenFields={[{ name: 'banner-id', value: banner.id }]}
-		>
-			<Trash2 size="0.8rem" />
-			<span>삭제</span>
-		</InlineActionForm>
-	</div>
-{/each}
-
 <style lang="scss">
+	.banner-manager {
+		gap: 0.3rem;
+		width: 100%;
+	}
+
 	h4 {
 		width: 100%;
 		font-weight: 500;
@@ -161,7 +175,7 @@
 		display: flex;
 		flex-direction: column;
 		gap: 0.3rem;
-		margin: 0.6rem 0 0;
+		margin: 0.3rem 0 0;
 		padding: 0;
 		width: 100%;
 		list-style: none;
@@ -178,8 +192,8 @@
 		img {
 			flex-shrink: 0;
 			border-radius: 0.3rem;
-			width: 7rem;
-			height: 2rem;
+			width: 6rem;
+			height: 1.8rem;
 			object-fit: cover;
 		}
 	}
@@ -204,10 +218,17 @@
 		}
 	}
 
+	.banner-actions {
+		display: flex;
+		flex-shrink: 0;
+		align-items: center;
+		gap: 0.3rem;
+		font-size: 0.7rem;
+	}
+
 	.info {
 		justify-content: flex-start;
-		margin-top: 0.6rem;
-		margin-bottom: 0.2rem;
+		margin-top: 0.3rem;
 		font-size: 0.7rem;
 	}
 
@@ -223,24 +244,8 @@
 		font-size: 0.7rem;
 	}
 
-	button {
+	button[type='submit'] {
 		margin-top: 0.6rem;
 		margin-left: auto;
-	}
-
-	// InlineActionForm 은 자체 form 을 만들어 위 폼 안에 넣을 수 없다.
-	.banner-actions {
-		display: flex;
-		align-items: center;
-		gap: 0.4rem;
-		margin-top: 0.3rem;
-		width: 100%;
-		font-size: 0.7rem;
-
-		& > span {
-			flex: 1;
-			min-width: 0;
-			color: var(--secondary-text);
-		}
 	}
 </style>
